0183f53b39ad0babb01e0b48c86d4d4163f4e0e7
BlueMoonL <79078980+BlueMoonL@users.noreply.github.com>
2022-02-14 18:46:22 +0900

    Simple_Tinder

---
+++ app/src/main/java/com/bluemoonl/ch13tinder/DBKey.kt
@@ -0,0 +1,13 @@
+package com.bluemoonl.ch13tinder
+
+class DBKey {
+    companion object {
+        const val USERS = "Users"
+        const val LIKED_BY = "likedBy"
+        const val LIKE = "like"
+        const val DIS_LIKE = "disLike"
+        const val NAME = "name"
+        const val USER_ID = "userId"
+        const val MATCH = "match"
+    }
+}
